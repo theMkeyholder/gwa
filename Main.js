857ba18b98fb11830e57300a5fa36eca6b986600
FlamemasterNXF <flamemasternxf@gmail.com>
2021-12-30 21:48:37 -0500

    Better Page System (subtabs)

--- Main.js
+++ Main.js
@@ -1,17 +1,47 @@
 let currentPage = 0
-let pageNumbers = [`0`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`10`,`11`,`12`,`13`,`14`,`15`,`16`,`17`,`18`,`19`,`20`,`21`,`22`,`23`,`24`,`25`,`26`,`27`,`28`,`29`,`30`,]
+let previousPage = 0
+let currentSubPage = 0
+let pageNumbers = [`0`,`1`,`2`,`3`,]
+let subPageNumbers = [`0`,`1`,]
 const pages = []
 for (let i=0;i<pageNumbers.length;i++){
     pages[i] = document.getElementById(`page${pageNumbers[i]}`)
 }
+const subPages = []
+for (let i=0;i<subPageNumbers.length;i++){
+    subPages[i] = document.getElementById(`subPage${subPageNumbers[i]}`)
+}
 function switchPage(x){
     pages[currentPage].style.display = `none`
+    previousPage = currentPage
     currentPage = x
+    currentSubPage = 0
+    for (let i=0;i<subPages.length;i++){subPages[i].style.display=`none`}
     pages[currentPage].style.display = `flex`
 }
-window.onload = function(){
-    switchPage(0)
-    for (let i=1;i<pageNumbers.length;i++){
-        pages[i].style.display = `none`
+function switchSubPage(x){
+    subPages[currentSubPage].style.display = `none`
+    currentSubPage = x
+    for (let i=0;i<pages.length;i++){pages[i].style.display=`none`}
+    subPages[currentSubPage].style.display = `flex`
+}
+function backPage(){
+    if (currentPage!==0){
+        if (currentSubPage===0){
+            pages[currentPage].style.display = `none`
+            currentPage = previousPage
+            currentSubPage = 0
+            for (let i=0;i<subPages.length;i++){subPages[i].style.display=`none`}
+            pages[currentPage].style.display = `flex`
+        }
+        else{
+            pages[currentPage].style.display = `flex`
+            currentSubPage = 0
+            for (let i=0;i<subPages.length;i++){subPages[i].style.display=`none`}
+        }
     }
+}
+window.onload = function(){
+    for (let i=1;i<pages.length;i++){pages[i].style.display = `none`}
+    for (let i=0;i<subPages.length;i++){subPages[i].style.display=`none`}
 }
